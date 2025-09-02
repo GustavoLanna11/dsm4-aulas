c272a12174ac3a6d17878b66213fc00db4e09adb
DSM4 <fatec-dsm4@academico.intra>
2025-09-01 21:42:13 -0300

    asasas

---
+++ LDW/ldw-python-flask/ldw-aula-03-integracao-api-publica/app.py
@@ -0,0 +1,11 @@
+# Do pacote importe Flask e o renderizador de páginas
+from flask import Flask, render_template, request
+from controllers import routes
+
+# Criando instância do Flask
+app = Flask(__name__, template_folder='views') # __name__ representa o nome da aplicação
+routes.init_app(app)
+
+# se for executado diretamente pelo interpretador
+if __name__ == '__main__':
+    app.run(host='localhost', port=4000, debug=True) #iniciando servidor
